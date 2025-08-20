798e99db0608d87281401e761252823c14e825f1
Loharanontsoanael <loharontsoasn@gmail.com>
2025-08-20 12:16:36 +0200

    creation du methode main dans Femme

--- src/iut/bad/Femme.java
+++ src/iut/bad/Femme.java
@@ -6,6 +6,12 @@ public class Femme extends Humain{
     public Femme(String nom, String prenom, int age) {
         super(nom,prenom,age);
     }
+    public static void main(String[] args) {
+        Homme homme = new Homme("Dupont", "Jean", 25);
+        Femme femme = new Femme("Martin", "Marie", 23);
 
+        homme.ami(femme);
+        femme.ami(homme);
+    }
 
 }
