24bafac08ce5c82b061f8108479d3d44a4dde6ad
Loharanontsoanael <loharontsoasn@gmail.com>
2025-08-20 12:06:39 +0200

    Tentative de déplacement de toString() , Resultats : Erreur ou problème car toString() est une méthode fondamentale d'Object, et les sous-classes perdraient l'accès aux attributs privés du parent

--- src/iut/bad/Humain.java
+++ src/iut/bad/Humain.java
@@ -20,7 +20,6 @@ public class Humain {
     public void details() {
         System.out.println(toString());
     }
-
     @Override
     public String toString() {
         return "Nom: " + nom + ", Prénom: " + prenom + ", Âge: " + age;
